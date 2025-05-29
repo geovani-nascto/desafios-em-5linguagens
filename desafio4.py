bfa0c3dcd900eae046422a6905ad5ebd90da7fd7
geovani-nascto <104122527+geovani-nascto@users.noreply.github.com>
2025-05-29 01:25:41 -0300

    att challenge python

--- desafio4.py
+++ desafio4.py
@@ -6,6 +6,11 @@ def calcular_imposto(salario):
         aliquota = 0.10
     else:
         aliquota = 0.15
-    return aliquota, salario
+    return aliquota * salario
 
 valor_salario = float(input("Valor do salário: "))
+valor_beneficio = float(input("Valor do benefício: "))
+
+valor_imposto = calcular_imposto(valor_salario)
+salario_final = valor_salario - valor_imposto + valor_beneficio
+print(f'{salario_final:.2f}')
